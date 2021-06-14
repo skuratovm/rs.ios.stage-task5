03ac72147da2b03c2dea049dabaf75d151d02fe0
skuratovm <maydibee@gmail.com>
2021-06-14 14:38:35 +0300

    feat:task 5 done:test passed.

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,15 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var output = 0
+               for i in 0..<prices.count{
+                   var max = 0
+                   for j in i + 1..<prices.count{
+                       if (prices[j] - prices[i] > max){max = prices[j] - prices[i]}
+                   }
+                   output += max
+               }
+               
+               return output
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -2,7 +2,38 @@ import UIKit
 
 class MessageDecryptor: NSObject {
     
-    func decryptMessage(_ message: String) -> String {
-        return ""
+func decryptMessage(_ message: String) -> String {
+    var counts = [String]()
+    var tempCount = ""
+    var repeatString = [String]()
+    var output = ""
+                
+    for char in message {
+        if char.isNumber {
+            tempCount = "\(tempCount)\(char)"
+        } else if char == "[" {
+        if tempCount == "" {
+            counts.append("1")
+        } else {
+            counts.append(tempCount)
+        }
+            tempCount = ""
+            repeatString.append(output)
+            output = ""
+        } else if char == "]" {
+            var tempString = ""
+        if let counts = Int(counts.popLast() ?? "1") {
+    for _ in 0..<counts {
+        tempString += output
+                            }
+                        }
+        output = repeatString.popLast()! + tempString
+        } else if char.isLetter {
+                output += String(char)
+                }
+            }
+                
+    return output
+
     }
 }
